96ba8d157e1fca8cd778415ef41882a0d8faa732
Jared Paquet <jaredpaquet@ufl.edu>
2025-10-06 10:27:25 -0500

    Update install_REEF_printer.sh

--- install_REEF_printer.sh
+++ install_REEF_printer.sh
@@ -6,6 +6,9 @@ read -s UFPASSWORD
 echo
 QUEUE=TSS-SRV-Print-2.ad.ufl.edu/ENG-REEF142-PRT-C7030_BW
 DEVICEURI=smb://$UFUSERNAME:$UFPASSWORD@ad.ufl.edu/$QUEUE
+echo 'Installing prerequisites'
+sudo apt install -y smbclient
+sudo systemctl restart cups
 smbclient -L //$QUEUE -U $UFUSERNAME@ad.ufl.edu%$UFPASSWORD &>/dev/null
 status=$?
 while [[ "$status" != "0" ]];
@@ -21,9 +24,6 @@ UFPASSWORD=$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]), "\n"' "$UFPASSWO
 echo 'Downloading drivers...'
 wget https://download.support.xerox.com/pub/drivers/VLC7000/drivers/linux/ar/VersaLink_C7000_5.739.0.0_PPD.zip
 unzip -p VersaLink_C7000_5.739.0.0_PPD.zip Linux/English/xrxC7030.ppd >xrxC7030.ppd
-echo 'Installing prerequisites'
-sudo apt install -y smbclient
-sudo systemctl restart cups
 echo 'Installing printer'
 sudo lpadmin -p REEFxerox -E -v "$DEVICEURI" -i './xrxC7030.ppd'
 echo 'Cleaning up files and enviroment variables'
